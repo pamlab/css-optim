39c7fca96300bcf688424553a442794ccbb2cb99
y_nihei <y_nihei@pam.co.jp>
2016-10-21 14:15:01 +0900

    remove: 使用していないライブラリを削除 update: gulp実行時にoutput内のファイルを削除,csscombを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var clean = require('gulp-clean');
 
-gulp.task('default', function () {
-  return gulp.src('./input/*')
+gulp.task('style', function () {
+  return gulp.src('./input/*.css')
+    .pipe(csscomb())
     .pipe(cleanCSS())
     .pipe(gulp.dest('./output'));
 });
+
+gulp.task('clean', function () {
+    return gulp.src('./output/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'style']);
